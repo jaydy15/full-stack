e56dfcdbf33f7b21114716da92b402bb054ef1b1
jay <dyjay15@gmail.com>
2020-09-28 13:18:49 +0800

    made the user register in the user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-// @route   GET api/auth
-// @desc    test route
+// @route   post api/user
+// @desc    Register User
 //@access   Pulic
-router.get('/', (req, res) => res.send('user Route'));
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Please enter 4 or more character').isLength({ min: 4 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log(req.body);
+    res.send('user Route');
+  }
+);
 
 module.exports = router;
