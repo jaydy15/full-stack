aac812942a4dbb2edc2ed2a5145c82cc14f70005
jay <dyjay15@gmail.com>
2020-10-01 13:11:22 +0800

    add login auth and logout link if user is login

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import Alerts from './components/layout/Alerts';
+import { loaduser } from './actions/auth';
+import setAuthToken from './utlis/setAuthToke';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loaduser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alerts />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,51 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from './../actions/type';
+
+const INITIAL_STATE = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+export default (state = INITIAL_STATE, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case LOGIN_FAIL:
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
